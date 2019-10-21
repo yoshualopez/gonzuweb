31c57f3f8edd0b4f038993919175d538b9b5a6c9
yell-yoshua <usuario1529yoshua@hotmail.com>
2019-10-20 21:51:24 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import ReactDOM from "react-dom";
+import React from "react";
+import AppValidation from "";
+
+ReactDOM.render(<AppValidation/>,document.getElementByid("App"));
---
+++ src/session.js
@@ -0,0 +1,9 @@
+import React,{Component} from "react";
+import axios from "axios";
+
+class AppValidation extends Component{
+    render(){
+        <div></div>
+    }
+}
+default export AppValidation;
